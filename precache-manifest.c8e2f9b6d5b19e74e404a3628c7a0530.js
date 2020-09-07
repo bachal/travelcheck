9e6816991ea6b871c464e97c27556470c5c8d4d2
somnathbachal023@gmail.com <somnathbachal023@gmail.com>
2020-09-07 17:52:48 +0530

    Updates

---
+++ precache-manifest.c8e2f9b6d5b19e74e404a3628c7a0530.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54e993fbe8fdb1703249cbd613cef9e",
+    "url": "/travelcheck/index.html"
+  },
+  {
+    "revision": "925ae6a7c71117c11b52",
+    "url": "/travelcheck/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "b7cfdc8eb0ec558fa06e",
+    "url": "/travelcheck/static/css/main.6c8554a5.chunk.css"
+  },
+  {
+    "revision": "925ae6a7c71117c11b52",
+    "url": "/travelcheck/static/js/2.335bfb48.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/travelcheck/static/js/2.335bfb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cfdc8eb0ec558fa06e",
+    "url": "/travelcheck/static/js/main.134758b3.chunk.js"
+  },
+  {
+    "revision": "3d63d226452520f87686",
+    "url": "/travelcheck/static/js/runtime-main.2d188706.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travelcheck/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21763f3037e921c71fab0276e70139d0",
+    "url": "/travelcheck/static/media/safari.21763f30.jpg"
+  }
+]);
---
+++ vehi_details.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { Carousel, Item, Caption, Card, Col, Figure, Button, ButtonGroup, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom';
+import './vehicle.css'
+import Dynamic_vehicles from './Dynamic_vehicles'
+import all_vehicle from './Dynamic_vehicles'
+
+export default function Vehi_details(props) {
+
+    return (
+        <div>
+            <div className="row">
+                <div className="col-sm-12 col-md-6 col-lg-6 bg-danger" style={{height:"300px"}}>
+
+
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-6">
+
+
+                </div>
+
+
+            </div>
+        </div>
+
+    )
+
+
+
+
+}
+
